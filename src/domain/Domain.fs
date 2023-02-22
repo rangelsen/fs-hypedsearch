3a38a788bda3771b206c0177aa98e687ceee8df7
rangelsen <roy.angelsen@gmail.com>
2023-02-22 13:57:24 +0100

    Declare spectrum and precursor type

--- src/domain/Domain.fs
+++ src/domain/Domain.fs
@@ -2,4 +2,17 @@
 
 [<AutoOpen>]
 module Domain =
-    type AminoAcid = | A | R | N | D | C | Q | E | G | H | I | L | K | M | F | P | O | S | U | T | W | Y | V | B | Z | X 
+    type AminoAcid = A | R | N | D | C | Q | E | G | H | I | L | K | M | F | P | O | S | U | T | W | Y | V | B | Z | X 
+
+    type Precursor = {
+        mass: double
+        charge: int
+    }
+
+    type Spectrum = {
+        id: string
+        mz: seq<double>
+        intensity: seq<double>
+        precursor: Precursor
+    }
+
