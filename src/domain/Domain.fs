d9a2decbba1d0e8a62c6b634cf31d410d3bd0e56
Roy Angelsen <roy.angelsen@elementlogic.no>
2023-03-04 10:29:23 +0100

    Create domain types for parsing input

--- src/domain/Domain.fs
+++ src/domain/Domain.fs
@@ -4,9 +4,28 @@
 module Domain =
     type AminoAcid = A | R | N | D | C | Q | E | G | H | I | L | K | M | F | P | O | S | U | T | W | Y | V | B | Z | X 
 
+    type Peptide = seq<AminoAcid>
+
+    // type Protein = seq<AminoAcid> ?
+
+    type Charge = int
+
+    type NormalizedFragment = {
+        // low
+        adjustedWeight: double
+        // high
+        charge: Charge
+    }
+
+    type Fragment = {
+        weight: double
+        abundance: double
+    }
+
     type Precursor = {
-        mass: double
-        charge: int
+        fragments: seq<Fragment>
+        weight: double
+        charge: Charge
     }
 
     type Spectrum = {
